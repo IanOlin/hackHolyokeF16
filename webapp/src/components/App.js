933fbebff4aae5ecd443ae8518af15a3deab7327
Ian Paul <ian.paul@students.olin.edu>
2016-11-05 09:08:25 -0400

    got the shit a little bit working

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import AudioPlayer from  'react-responsive-audio-player';
 
+let soundbite = 'test/audiotest.ogg'
+let text = 'This will be useful'
+
+var playlist =
+[{ url: soundbite, displayText: text }];
+
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
 			<AudioPlayer playlist={playlist} autoplay={true} />
-			Hello Olin!
 			</div>
 		);
 	}
