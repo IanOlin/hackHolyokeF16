0e10db6e769f4967f40a95d6dcf76cb9457e9248
Ian Paul <ian.paul@students.olin.edu>
2016-11-05 14:57:16 -0400

    pushing sam's cheating bs code

--- webapp/src/components/SoundBytesContainer.js
+++ webapp/src/components/SoundBytesContainer.js
@@ -1,3 +1,5 @@
+import '../styles/soundbyte-container.scss';
+
 import React from 'react';
 
 import SoundByteBox from './SoundByteBox';
@@ -6,6 +8,9 @@ export default class SoundBytesContainer extends React.Component {
 	render() {
 		return (
 			<div className="soundbytes-container">
+				<div className="title">
+					8Byt - All Sounds
+				</div>
 				{this.props.soundbytes.map((soundbyte) =>
 					<SoundByteBox key={soundbyte.id}{...soundbyte}></SoundByteBox>
 				)}
