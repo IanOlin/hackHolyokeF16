5249c6271d1c5a42c26080172da011636cf29ea7
margaretmcrawf <margaretmcrawf@gmail.com>
2016-11-05 12:17:29 -0400

    maybe adding sound files works

--- server/eightbyte/src/main/java/olin/eightbyte/resources/SoundByteResource.java
+++ server/eightbyte/src/main/java/olin/eightbyte/resources/SoundByteResource.java
@@ -1,5 +1,11 @@
 package olin.eightbyte.resources;
 
+import java.io.File;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -10,6 +16,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import javax.mail.MessagingException;
 
 import olin.eightbyte.api.SoundByte;
 import olin.eightbyte.db.SoundByteDAO;
@@ -20,7 +32,6 @@ import olin.eightbyte.db.SoundByteDAO;
 public class SoundByteResource {
 	SoundByteDAO dao;
 	
-
 	public SoundByteResource(SoundByteDAO dao) {
 		this.dao = dao;
 	}
@@ -40,5 +51,37 @@ public class SoundByteResource {
 	@Path("/submit")
 	public void add(@Valid SoundByte soundByte) {
 		dao.addSoundByte(soundByte);
+	}
+	
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+	        @FormDataParam("file") InputStream uploadedInputStream,
+	        @FormDataParam("file") FormDataContentDisposition fileDetail) throws MessagingException, IOException {
+
+	    String uploadedFileLocation = "eightbyte/src/main/resources/sounds/" + fileDetail.getFileName();
+
+	    // save it
+	    writeToFile(uploadedInputStream, uploadedFileLocation);
+	    String output = "File uploaded to : " + uploadedFileLocation;
+    return Response.ok(output).build();
+	}
+	
+//	    @POST
+//	    public void uploadFile(){
+//	            System.out.println("In upload");
+//	    }
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) throws IOException {
+	    int read;
+	    final int BUFFER_LENGTH = 1024;
+	    final byte[] buffer = new byte[BUFFER_LENGTH];
+	    OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
+	    while ((read = uploadedInputStream.read(buffer)) != -1) {
+	        out.write(buffer, 0, read);
+	    }
+	    out.flush();
+	    out.close();
 	}
 }
