94226a5cc4a124017d8539349f50b56a49f13d31
sammyers <sam@students.olin.edu>
2016-11-04 21:12:04 -0400

    Scaffolded Dropwizard and React apps.

---
+++ webapp/webpack.config.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+
+var BUILD_DIR = path.resolve(__dirname, './build');
+var APP_DIR = path.resolve(__dirname, './src');
+
+var config = {
+    devtool: 'eval',
+    entry: [
+        'whatwg-fetch',
+        'webpack-dev-server/client?http://localhost:9090',
+        'webpack/hot/only-dev-server',
+        APP_DIR + '/index'
+    ],
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loaders: [
+                    'react-hot',
+                    'babel'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                include: APP_DIR,
+                loaders: [
+                    'style',
+                    'css',
+                    'postcss',
+                    'sass?outputStyle=expanded'
+                ]
+            }
+        ]
+    },
+    postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
+    devServer: {
+        hot: true,
+        contentBase: BUILD_DIR
+    }
+};
+
+module.exports = config;
