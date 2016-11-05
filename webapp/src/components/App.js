0e114ffa9dcf2567dca384eb78adbb87ddbc6f38
Ian Paul <ian.paul@students.olin.edu>
2016-11-05 09:08:25 -0400

    got the shit a little bit working

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import AudioPlayer from  'react-responsive-audio-player';
 
-var playlist =
-[{ url: 'audiotest.ogg', displayText: 'Track 1 - a track to remember' },
-{ url: 'audiotest.ogg', displayText: 'Oggs Oggs Oggs' }];
+let soundbite = 'test/audiotest.ogg'
+let text = 'This will be useful'
 
-var reactElement = React.createElement(AudioPlayer, {
-	playlist: playlist,
-	autoplay: true,
-	autoplayDelayInSeconds: 2.1,
-	style: { position: 'fixed', bottom: 0 }
-})
+var playlist =
+[{ url: soundbite, displayText: text }];
+
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
 			<AudioPlayer playlist={playlist} autoplay={true} />
-			Hello Olin!
 			</div>
 			);
 	}
