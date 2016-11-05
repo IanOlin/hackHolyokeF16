a7af846a0a8ffc1853cf40470e522b16efc7fdc9
Ian Paul <ian.paul@students.olin.edu>
2016-11-05 08:22:26 -0400

    pushing for sam to check some bs

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import AudioPlayer from  'react-responsive-audio-player';
-// import ReactDOM from 'react-dom';
 
 var playlist =
-[{ url: 'test/audiotest.ogg',
-displayText: 'Track 1 by Some Artist' },
-{ url: 'test/audiotest.ogg',
-displayText: 'Some Other Artist - Track 2' }];
+[{ url: 'audiotest.ogg', displayText: 'Track 1 - a track to remember' },
+{ url: 'audiotest.ogg', displayText: 'Oggs Oggs Oggs' }];
 
-// ReactDOM.render(
-// 	<AudioPlayer playlist={playlist} hideBackSkip={true} />,
-// 	document.getElementById('audio_player_container')
-// 	);
+var reactElement = React.createElement(AudioPlayer, {
+	playlist: playlist,
+	autoplay: true,
+	autoplayDelayInSeconds: 2.1,
+	style: { position: 'fixed', bottom: 0 }
+})
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-			<AudioPlayer playlist={playlist} hideBackSkip={true} />
+			<AudioPlayer playlist={playlist} autoplay={true} />
 			Hello Olin!
 			</div>
 			);
